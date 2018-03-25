c21bffc377185427d71b725470a09af238060bc9
Lucia <luxiyalu@foxmail.com>
2018-03-25 15:15:25 -0700

    refactoring get method

--- douban/NotesController.js
+++ douban/NotesController.js
@@ -7,16 +7,14 @@ router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json());
 
 router.get('/:user', async (req, res) => {
-  const userHandle = req.params.user;
-  const numOfBooks = req.query.size;
-
   try {
-    const noteExport = new NotesExportService(userHandle);
-    const bookNotesHTMLArr = await noteExport.getBooksNotesHTML(numOfBooks);
-    const noteParse = new NoteParseService(bookNotesHTMLArr);
-    const booksArr = noteParse.getBooksNotes();
+    const noteExportInstance = new NotesExportService(req.params.user);
+    const booksHTMLArr = await noteExportInstance.getBooksNotesHTML(req.query.size);
+    const booksParseInstances = booksHTMLArr.map(bookHTML => new BookParseService(bookHTML));
+    const booksParsePromises = booksParseInstances.map(async parse => parse.export());
+    const parsedBooks = await Promise.all(booksParsePromises);
 
-    res.status(200).send(booksArr);
+    res.status(200).send(parsedBooks);
   } catch (error) {
     res.status(500).send(error);
   }
