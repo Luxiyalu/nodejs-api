aa5415e3f89dc081e94e34b9ae9ee91306e8c2f2
Lucia <luxiyalu@foxmail.com>
2018-03-25 16:11:03 -0700

    Instead of parsing into an object, just return the HTML

--- douban/NotesController.js
+++ douban/NotesController.js
@@ -11,11 +11,14 @@ router.get('/:user', async (req, res) => {
   try {
     const noteExportInstance = new NotesExportService(req.params.user);
     const booksHTMLArr = await noteExportInstance.getBooksNotesHTML(req.query.size);
-    const booksParseInstances = booksHTMLArr.map(bookHTML => new BookParseService(bookHTML));
-    const booksParsePromises = booksParseInstances.map(async parse => parse.export());
-    const parsedBooks = await Promise.all(booksParsePromises);
+    // const booksParseInstances = booksHTMLArr.map(bookHTML => new BookParseService(bookHTML));
+    // const booksParsePromises = booksParseInstances.map(async parse => parse.export());
+    // const parsedBooks = await Promise.all(booksParsePromises);
 
-    res.status(200).send({books: parsedBooks});
+    res.status(200).send({
+      booksHTMLArr: booksHTMLArr,
+      // books: parsedBooks,
+    });
   } catch (error) {
     res.status(500).send(error);
   }
